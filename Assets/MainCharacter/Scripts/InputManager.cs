2e34ab3dc7eb08e2a111a8fa64d369098bd37346
leonogusu <figer97s.i.x.x@gmail.com>
2020-06-06 01:59:03 +0900

    コントローラー入力、カメラ実装

---
+++ Assets/MainCharacter/Scripts/InputManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace R2
+{
+	public class InputManager : StateAction
+	{
+		PlayerStateManager s;
+
+		//Triggers & bumpers
+		bool Rb, Rt, Lb,Lt,isAttacking, b_Input, y_Input, x_Input, a_Input, inventoryInput,
+		leftArrow, rightArrow, upArrow, downArrow;
+	
+		public InputManager(PlayerStateManager states)
+		{
+			s = states;
+		}
+
+		public override bool Execute()
+		{
+			bool retVal = false;
+			isAttacking = false;
+
+            s.horizontal = Input.GetAxis("Horizontal");
+            s.vertical = Input.GetAxis("Vertical");
+            Rb = Input.GetButton("RB");
+            Rt = Input.GetButton("RT");
+            Lb = Input.GetButton("LB");
+            Lt = Input.GetButton("LT");
+            inventoryInput = Input.GetButton("Inventory");
+            a_Input = Input.GetButton("A");
+            b_Input = Input.GetButton("B");
+            y_Input = Input.GetButtonDown("Y");
+            x_Input = Input.GetButton("X");
+            /*leftArrow = Input.GetButton("Left");
+			rightArrow = Input.GetButton("Right");
+			upArrow = Input.GetButton("Up");
+			downArrow = Input.GetButton("Down");*/
+            s.mouseX = Input.GetAxis("mouse X");
+            s.mouseY = Input.GetAxis("mouse Y");
+
+            s.moveAmount = Mathf.Clamp01(Mathf.Abs(s.horizontal) + Mathf.Abs(s.vertical));
+
+			retVal = HandleAttacking();
+		
+
+			return retVal;
+		}
+
+		bool HandleAttacking()
+		{
+			if (Rb || Rt || Lb || Lt)
+			{
+				isAttacking = true;
+			}
+
+			if (y_Input)
+			{
+				isAttacking = false;
+			}
+
+			if (isAttacking)
+			{
+				//Find the actual attack animation from the items etc.
+				//play animation
+				s.PlayTargetAnimation("Attack 1", true);
+				s.ChangeState(s.attackStateId);
+			}
+
+			return isAttacking;
+		}
+
+		
+	}
+}
---
+++ Assets/MainCharacter/Scripts/PlayerStateManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace R2
+{
+	public class PlayerStateManager : CharacterStateManager
+	{
+		[Header("Inputs")]
+		public float mouseX;
+		public float mouseY;
+		public float moveAmount;
+		public Vector3 rotateDirection;
+		
+
+		[Header("References")]
+		public new Transform camera;
+        public Transform target;
+
+		[Header("Movement Stats")]
+		public float frontRayOffset = .5f;
+		public float movementSpeed = 1;
+		public float adaptSpeed = 1;
+		public float rotationSpeed = 10;
+
+		[HideInInspector]
+		public LayerMask ignoreForGroundCheck;
+
+		public string locomotionId = "locomotion";
+		public string attackStateId = "attackId";
+
+		public override void Init()
+		{
+			base.Init();
+
+			State locomotion = new State(
+				new List<StateAction>() //Fixed Update
+				{
+					new MovePlayerCharacter(this)
+				},
+				new List<StateAction>() //Update
+				{
+					new InputManager(this),
+				},
+				new List<StateAction>()//Late Update
+				{
+				}
+				);
+
+			locomotion.onEnter = DisableRootMotion;
+
+			State attackState = new State(
+				new List<StateAction>() //Fixed Update
+				{
+				},
+				new List<StateAction>() //Update
+				{
+					new MonitorInteractingAnimation(this,"isInteracting", locomotionId),
+				},
+				new List<StateAction>()//Late Update
+				{
+				}
+				);
+
+			attackState.onEnter = EnableRootMotion;
+
+			RegisterState(locomotionId, locomotion);
+			RegisterState(attackStateId, attackState);
+
+			ChangeState(locomotionId);
+
+			ignoreForGroundCheck = ~(1 << 9 | 1 << 10);
+
+		}
+
+		private void FixedUpdate()
+		{
+			delta = Time.fixedDeltaTime;
+
+			base.FixedTick();
+		}
+
+		private void Update()
+		{
+			delta = Time.deltaTime;
+			base.Tick();
+		}
+
+		private void LateUpdate()
+		{
+			base.LateTick();
+		}
+
+		#region State Events
+		void DisableRootMotion()
+		{
+			useRootMotion = false;
+		}
+
+		void EnableRootMotion()
+		{
+			useRootMotion = true;
+		}
+
+		#endregion
+
+	}
+}
